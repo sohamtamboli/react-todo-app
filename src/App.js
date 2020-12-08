2d51e35eef3e2471f1f394630596da8fe23b22b7
Soham Tamboli <sohamtamboli@gmail.com>
2020-12-08 12:36:16 +0530

    Revert "Revert "added input field""
    This reverts commit e22343c2a908d79b8a13d1ce2a534d68cff9bf01.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='header'>
+				<h1> Your Todos </h1>
+			</div>
+			<div className='list-container'>
+				<TodoInput />
+			</div>
+		</div>
+	);
 }
 
 export default App;
