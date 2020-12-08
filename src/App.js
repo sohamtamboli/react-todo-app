19807d53f0eef057da3e1fcc48bbdf11c4839702
Soham Tamboli <sohamtamboli@gmail.com>
2020-12-08 21:51:58 +0530

    Added link to github repository

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
 		<div className='app'>
 			<div className='header'>
 				<h1> Your Todos </h1>
+				<a
+					className='github-link'
+					href='https://github.com/sohamtamboli/react-todo-app'>
+					<box-icon size='lg' color='#fff' type='logo' name='github'></box-icon>
+				</a>
 			</div>
 			<div className='list-container'>
 				<TodoInput
